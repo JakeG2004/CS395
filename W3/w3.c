a325e86364a31fae042437993d374173b6733b31
JakeG2004 <jake.lgendreau@gmail.com>
2025-01-16 16:25:57 -0800

    3 spaces

--- W3/w3.c
+++ W3/w3.c
@@ -13,26 +13,26 @@ int PowN(int n);
 
 int main(int argc, char* argv[])
 {
-    // Validate arguments
-    if(argc != 2)
-    {
-        printf("ERROR: Need 1 argument!\n");
-        exit(-1);
-    }
+   // Validate arguments
+   if(argc != 2)
+   {
+      printf("ERROR: Need 1 argument!\n");
+      exit(-1);
+   }
 
-    int n = atoi(argv[1]);
+   int n = atoi(argv[1]);
 
-    int res = PowN(n);
-    
-    printf("%i\n", res);
+   int res = PowN(n);
+
+   printf("%i\n", res);
 }
 
 int PowN(int n)
 {
-    if(n <= 0)
-    {
-        return 1;
-    }
+   if(n <= 0)
+   {
+      return 1;
+   }
 
-    return(PowN(n - 1) + PowN(n - 1));
+      return(PowN(n - 1) + PowN(n - 1));
 }
